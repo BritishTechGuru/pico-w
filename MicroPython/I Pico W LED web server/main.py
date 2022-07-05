d1a1aff6639f70678b36eed6753a04e61e339e54
Hermann-SW <hermann@stamm-wilbrandt.de>
2022-07-05 12:44:52 +0200

    Remove server side processing, add static file webserver capability

--- MicroPython/I Pico W LED web server/main.py
+++ MicroPython/I Pico W LED web server/main.py
@@ -10,12 +10,9 @@ ap.active(True)
 led = machine.Pin('LED', machine.Pin.OUT)
 led.off()
 
-# Function to load in html page    
-def get_html(html_name):
-    with open(html_name, 'r') as file:
-        html = file.read()
-        
-    return html
+def get_file(file_name):
+    with open(file_name, 'r') as file:
+        return file.read()
 
 # HTTP server with socket
 addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
@@ -30,28 +27,24 @@ print('Listening on', addr)
 while True:
     try:
         cl, addr = s.accept()
-        print('Client connected from', addr)
-        r = cl.recv(1024)
-        print(r)
+        led.on()
+#        print('Client connected from', addr)
+        r = str(cl.recv(1024))
+#        print(r)
+        r = r[r.find("/"):]
+        r = r[1:r.find(" ")]
+        if (r == ""):
+            r = "index.html"
+
+#        print(r)
         
-        r = str(r)
-        led_on = r.find('?led=on')
-        led_off = r.find('?led=off')
-        print('led_on = ', led_on)
-        print('led_off = ', led_off)
-        if led_on == 10:
-            print('LED ON')
-            led.value(1)
-            
-        if led_off == 10:
-            print('LED OFF')
-            led.value(0)
-            
-        response = get_html('index.html')
+        response = get_file(r)
         cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
         cl.send(response)
         cl.close()
+        led.off()
         
     except OSError as e:
         cl.close()
-        print('Connection closed')
+#        print('Connection closed')
+        led.off()
